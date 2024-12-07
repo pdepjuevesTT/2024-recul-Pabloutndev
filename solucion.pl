ceed8b86d3ca5167fed36c7b64e09dc3137e18ee
Pabloutndev <plarocca@frba.utn.edu.ar>
2024-12-07 09:32:32 -0300

    primer commit

--- solucion.pl
+++ solucion.pl
@@ -1,1 +1,22 @@
-% Solución en Prolog
+% Log Home Inspections
+% Legajo: 1762310
+% Pablo Nicolas La Rocca
+
+% Punto 1
+% vive(Persona, Ambiente)
+% functores: casa(m^2) depa(ambientes,banios) loft(anio)
+vive(juan,casa(120)).
+vive(nico,depa(3,2)).
+vive(alf,depa(3,1)).
+vive(julian,loft(2000)).
+vive(vale,depa(4,1)).
+vive(fer,110).
+vive(rocio,casa(90)).
+
+vivenEn([alf,juan,nico,julian],almagro).
+vivenEn([vale,fer],flores).
+
+% Punto 2
+esCopado(Persona, casa(Metros)) :-
+    vive(Persona,)
+    Metros > 100.
